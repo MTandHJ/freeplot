95aa4c3031d6f4bfca5980b14ce4eb83b948a790
MTandHJ <congxueric@gmail.com>
2021-10-28 16:43:21 +0800

    add import/export_pickle

--- freeplot/utils.py
+++ freeplot/utils.py
@@ -1,7 +1,8 @@
 
-from typing import Dict, Iterable, List
+from typing import Dict, Iterable, List, NoReturn
 import json
 import inspect
+import pickle
 import matplotlib.pyplot as plt
 from .config import style_cfg
 
@@ -10,29 +11,29 @@ def load(filename: str) -> Dict:
         data = json.load(j)
     return data
 
+def export_pickle(data: Dict, file_: str) -> NoReturn:
+    fh = None
+    try:
+        fh = open(file_, "wb")
+        pickle.dump(data, fh, pickle.HIGHEST_PROTOCOL)
+    except (EnvironmentError, pickle.PicklingError) as err:
+        ExportError_ = type("ExportError", (Exception,), dict())
+        raise ExportError_(f"Export Error: {err}")
+    finally:
+        if fh is not None:
+            fh.close()
 
-def axis(func):
-    def wrapper(*args, **kwargs):
-        axis = kwargs[axis]
-        results = func(*args, **kwargs)
-        newresults = dict()
-        for name, value in results.item():
-            newresults[axis + results] = value
-        return value
-    wrapper.__name__ = func.__name__
-    wrapper.__doc__ = func.__doc__
-    return wrapper
+def import_pickle(file_: str) -> Dict:
+    fh = None
+    try:
+        fh = open(file_, "rb")
+        return pickle.load(fh)
+    except (EnvironmentError, pickle.UnpicklingError) as err:
+        raise ImportError(f"Import Error: {err}")
+    finally:
+        if fh is not None:
+            fh.close()
 
-def reset(set):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            results = func(*args, **kwargs)
-            results[index] = kwargs[index]
-            return set(*results)
-        wrapper.__name__ = func.__name__
-        wrapper.__doc__ = func.__doc__
-        return wrapper
-    return decorator
 
 def getmore(doc):
     def decorator(func):
@@ -46,6 +47,7 @@ def getmore(doc):
         wrapper.__doc__ = doc
         return wrapper
     return decorator
+
 
 def get_style(style_type):
     assert style_type is not None, "style should not be None ..."
