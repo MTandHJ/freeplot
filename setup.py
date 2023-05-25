46b7bf7f0d90df571578b88a8421eac9f9776ece
MTandHJ <congxueric@gmail.com>
2023-05-25 13:27:45 +0800

    version: 0.3.3

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ with open("README.md", "r") as fh:
   long_description = fh.read()
 
 requires = [
-    'numpy>=1.18.1',
-    'pandas>=1.0.1',
-    'scipy>=1.4.1',
-    'scikit-learn>=0.23.2',
-    'matplotlib==3.1.3',
-    'seaborn==0.10.0',
+    'numpy==1.20.3',
+    'pandas==1.3.4',
+    'matplotlib==3.4.3',
+    'seaborn>=0.10.0',
     'SciencePlots==1.0.9'
 ]
 
